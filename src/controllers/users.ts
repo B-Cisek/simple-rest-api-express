aee0d5134f04935fbf392d69844c949433c8b5df
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2023-11-10 00:15:43 +0100

    push commit

---
+++ src/controllers/users.ts
@@ -0,0 +1,46 @@
+import { getUsers, deleteUserById, getUserById } from '../db/users';
+import { Request, Response } from 'express';
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await getUsers();
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const deleteUser = await deleteUserById(id);
+
+    return res.json(deleteUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
